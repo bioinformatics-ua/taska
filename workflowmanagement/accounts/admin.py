d98cb4c6aad2b1e367bede43221b5ead322904e1
Ricardo Ribeiro <ribeiro.r@ua.pt>
2015-05-08 15:53:50 +0100

    Some improvements on profile creation. - Added profile model to django admin so we can edit profiles from there.

--- workflowmanagement/accounts/admin.py
+++ workflowmanagement/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from models import Profile
+
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    ''' Django-Admin page for listing, adding and editing :class:`accounts.models.Profile` entries.
+    '''
+    pass
--- workflowmanagement/accounts/models.py
+++ workflowmanagement/accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.dispatch import receiver
 
 from django.contrib.auth.models import User
 
@@ -13,3 +14,18 @@ class Profile(models.Model):
     user        = models.OneToOneField(User)
     detail_mode = models.PositiveSmallIntegerField(choices=DETAIL_MODES, default=POPUP)
     notification= models.BooleanField(default=False)
+
+    def __str__(self):
+        return "User profile for %s" % self.user
+
+@receiver(models.signals.post_save, sender=User)
+def __generate_profile(sender, instance, created, *args, **kwargs):
+    '''This method uses the post_save signal to automatically generate unique public hashes to be used when referencing an process.
+    '''
+    if created:
+        try:
+            profile = instance.profile
+
+        except Profile.DoesNotExist:
+            p = Profile(user=instance)
+            p.save()
