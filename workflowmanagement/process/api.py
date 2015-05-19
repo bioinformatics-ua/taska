706e24925273c4a8a0135f704104071bf1877b19
Ricardo Ribeiro <ribeiro.r@ua.pt>
2015-05-19 13:52:38 +0100

    - Fixed bug with my processtasks listing not ordering yet.

--- workflowmanagement/process/api.py
+++ workflowmanagement/process/api.py
@@ -485,7 +485,17 @@ class MyProcessTaskUserDetailSerializer(ProcessTaskUserSerializer):
 class MyTasks(generics.ListAPIView):
     queryset = ProcessTaskUser.objects.none()
     serializer_class = MyProcessTaskUserSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (filters.DjangoFilterBackend, AliasOrderingFilter)
+    ordering_fields = ('user', 'title', 'task', 'task_repr', 'type', 'deadline', 'reassigned', 'reassign_date', 'finished', 'process','processtask')
+    ordering_map = {
+        'task': 'processtask__task__hash',
+        'process': 'processtask__process__hash',
+        'processtask': 'processtask_hash',
+        'title': 'processtask__task__title',
+        'deadline': 'processtask__deadline',
+        'type': 'processtask__task__ttype',
+        'task_repr': 'processtask__task__title'
+    }
 
     def get_queryset(self):
         """
