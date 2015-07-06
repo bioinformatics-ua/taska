0bb60c60f6808bd9eba071354c1435035bca0037
Ricardo Ribeiro <ribeiro.r@ua.pt>
2015-07-06 20:49:15 +0100

    Trying to fix #56

--- workflowmanagement/ui/static/js/components/reusable/workflow.jsx
+++ workflowmanagement/ui/static/js/components/reusable/workflow.jsx
@@ -104,8 +104,8 @@ const WorkflowTable = React.createClass({
     return  <div className="panel panel-default panel-overflow griddle-pad">
               <div className="panel-heading">
                 <i className="fa fa-sitemap pull-left"></i>
-                <h3 style={{position: 'absolute', width: '95%'}} className="text-center panel-title">My Protocols</h3>
-                <Link to="WorkflowEdit" params={{object: 'add', mode: 'edit'}} className="pull-right btn btn-xs btn-success"><i className="fa fa-plus"></i></Link>
+                <h3 className="text-center panel-title">My Protocols</h3>
+                <Link style={{position: 'absolute', right: '10px', top: '7px', zIndex: 1002}} to="WorkflowEdit" params={{object: 'add', mode: 'edit'}} className="pull-right btn btn-xs btn-success"><i className="fa fa-plus"></i></Link>
               </div>
               <Griddle
                   noDataMessage={<center>You have not created any protocols yet, click on the plus icon above to create a new protocol.</center>}
